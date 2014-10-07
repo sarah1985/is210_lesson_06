9702390f2761d6f5ed5ce5d01f8f701bddf6c5ed
sarah1985 <sarahmleavy@gmail.com>
2014-10-06 20:54:47 -0400

    task 3

--- task_03.py
+++ task_03.py
@@ -1,1 +1,31 @@
-__author__ = 'Sarah'
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Task 03: Sorting Data """
+
+import data
+
+
+def bubble_sort(list_input):
+
+    swapped = False
+    first_run = True
+
+    while first_run or swapped:
+
+        swapped = False
+        first_run = False
+
+        for i in range(1, len(list_input)):
+
+            if list_input[i - 1] > list_input[i]:
+
+                a = list_input[i - 1]
+                list_input[i-1] = list_input[i]
+                list_input[i] = a
+                swapped = True
+
+    return list_input
+
+
+#a = [5, 1, 9, 8, 6, 3, 0]
+#sprint bubble_sort(a)
